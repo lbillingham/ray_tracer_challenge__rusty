dff61408e05b6dafb25e0705ec37cfaa47e642a3
Laurence Billingham <laurence@travelnest.com>
2020-04-07 17:25:03 +0100

    :sparkles: add projectile motion playground

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,46 @@ extern crate approx;
 
 pub mod tuple;
 
+#[derive(Debug, Copy, Clone)]
+struct Projectile {
+    pub position: tuple::Tuple,
+    pub velocity: tuple::Tuple,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Environment {
+    pub gravity: tuple::Tuple,
+    pub wind: tuple::Tuple,
+}
+
+fn tick(env: Environment, proj: Projectile) -> Projectile {
+    let pos = proj.position + proj.velocity;
+    let vel = proj.velocity + env.gravity + env.wind;
+    Projectile {
+        position: pos,
+        velocity: vel,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p = Projectile {
+        position: tuple::point(0., 1., 0.),
+        velocity: tuple::vector(1., 1., 0.).normalize(),
+    };
+    let e = Environment {
+        gravity: tuple::vector(0., -0.1, 0.),
+        wind: tuple::vector(-0.01, 0., 0.),
+    };
+    while p.position.y > 0. && p.position.y < 5.0 {
+        println!(
+            "Position {:.3}, {:.3}, {:.3}",
+            p.position.x, p.position.y, p.position.z
+        );
+        println!(
+            "Velocity {:.3}, {:.3}, {:.3}",
+            p.velocity.x, p.velocity.y, p.velocity.z
+        );
+        println!("------------------------------");
+        p = tick(e, p);
+    }
 }
