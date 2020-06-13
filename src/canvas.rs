a639350b839c4a78d6e6dd15097f8b7538526865
Laurence Billingham <laurence@travelnest.com>
2020-06-13 16:13:27 +0100

    :sparkles: :white_check_mark: write body of ppm file to sting buffer

--- src/canvas.rs
+++ src/canvas.rs
@@ -4,7 +4,7 @@ use std::ops::{Index, IndexMut};
 #[cfg(test)]
 mod tests_for_canvas {
     use crate::canvas::*;
-    use crate::color::RED;
+    use crate::color::{color, RED};
 
     #[test]
     fn it_can_be_instatiated_with_a_width_and_height() {
@@ -63,9 +63,25 @@ mod tests_for_canvas {
     #[test]
     fn it_can_write_the_header_of_a_ppm_file() {
         let c = canvas(5, 3);
-        let ppm = c.to_ppm();
+        let ppm = c._ppm_header();
         let ppm_header = ppm.lines().take(3).collect::<Vec<&str>>().join("\n");
         assert_eq!(ppm_header, "P3\n5 3\n255");
+    }
+
+    #[test]
+    fn it_can_write_the_pixels_of_a_ppm_file() {
+        let mut canv = canvas(5, 3);
+        let red = color(1.5, 0., 0.);
+        let green = color(0., 0.5, 0.);
+        let blue = color(-0.5, 0., 1.);
+        canv[0][0] = red;
+        canv.write_pixel(2, 1, green);
+        write_pixel(&mut canv, 4, 2, blue);
+        let ppm_body = canv._ppm_body();
+        let expected = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n\
+                        0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n\
+                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 255";
+        assert_eq!(expected, ppm_body);
     }
 }
 
@@ -88,12 +104,6 @@ impl IndexMut<usize> for PpmRowBytes {
     }
 }
 
-impl PpmRowBytes {
-    pub fn new(size: usize) -> Self {
-        Self(vec![0 as u8; size])
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct Canvas {
     width: usize,
@@ -102,6 +112,14 @@ pub struct Canvas {
 }
 
 impl Canvas {
+    fn _pixels_bytes(&self) -> usize {
+        3
+    }
+
+    fn _buffer_size(&self) -> usize {
+        self.width * self.height * self._pixels_bytes()
+    }
+
     fn _ppm_header(&self) -> String {
         format!(
             "P3\n{width} {height}\n{max_color}\n",
@@ -111,27 +129,45 @@ impl Canvas {
         )
     }
 
-    fn _ppm_pixels(&self) -> PpmRowBytes {
-        const PIXELS_BYTES: usize = 3;
-        let mut bytes = PpmRowBytes::new(self.width * self.height * PIXELS_BYTES);
+    fn _ppm_pixels(&self) -> Vec<u8> {
+        let mut bytes = vec![0u8; self._buffer_size()];
         let mut i_rgb = 0;
         for row in &self.pixels {
             for pixel in row {
                 bytes[i_rgb] = clamp_to_byte(pixel.red);
                 bytes[i_rgb + 1] = clamp_to_byte(pixel.green);
                 bytes[i_rgb + 2] = clamp_to_byte(pixel.blue);
-                i_rgb += PIXELS_BYTES;
+                i_rgb += self._pixels_bytes();
             }
         }
         bytes
     }
 
+    fn _ppm_body(&self) -> String {
+        let mut s = String::new();
+        let px_bytes = self._ppm_pixels();
+        for (i, u) in px_bytes.iter().enumerate() {
+            let f = format!("{digit}", digit = u);
+            s.push_str(&f);
+            let sep = self._pixel_output_seperator(i);
+            s.push_str(sep);
+        }
+        s
+    }
+
     pub fn to_ppm(&self) -> String {
         let mut s = self._ppm_header();
-        let px_bytes = self._ppm_pixels();
-        let px_string = format!("{:#?}", px_bytes);
-        s.push_str(&px_string);
+        s.push_str(&self._ppm_body());
         s
+    }
+
+    fn _pixel_output_seperator(&self, idx: usize) -> &str {
+        let n_row = self._pixels_bytes() * self.width;
+        match idx {
+            idx if ((idx + 1) >= self._buffer_size()) => "",
+            idx if (idx + 1) % n_row == 0 => "\n",
+            _ => " ",
+        }
     }
 
     pub fn pixel_at(self, column_index: usize, row_index: usize) -> Color {
